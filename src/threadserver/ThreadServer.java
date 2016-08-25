9336bd2576012b8c636f8b468eb8c16f2cefb92f
Simba <Simba@ProBook>
2016-08-25 14:12:27 +0200

    First version. Added comments.

--- src/threadserver/ThreadServer.java
+++ src/threadserver/ThreadServer.java
@@ -1,33 +1,20 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package threadserver;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-
-public class ThreadServer {
-
-    public static void main(String[] args) 
+/**
+ *
+ * @author Simba
+ */
+public class ThreadServer 
+{
+    public static void main(String[] args)
     {
-        int portNumber = 6999;
-        
-        try(ServerSocket serverSocket=new ServerSocket(portNumber))
-        {
-            while(true)
-            {
-                try
-                {
-                    new ThreadSocket(serverSocket.accept()).start();
-                }
-                catch(Exception ex)
-                {
-                    System.out.println(ex.toString());
-                }
-            }
-        }
-        catch(IOException ex)
-        {
-            System.out.println(ex.toString());
-        }
+        MainPage mp=new MainPage();
+        mp.setVisible(true);
     }
     
 }
